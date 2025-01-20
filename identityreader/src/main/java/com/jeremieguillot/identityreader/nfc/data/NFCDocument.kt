56dbd54e7d9550e894446d21108a46b9de9f8a99
Jeremie Guillot <jeremieguillot.pro@gmail.com>
2025-01-20 15:01:27 +0100

    Improve doc

--- identityreader/src/main/java/com/jeremieguillot/identityreader/nfc/data/NFCDocument.kt
+++ identityreader/src/main/java/com/jeremieguillot/identityreader/nfc/data/NFCDocument.kt
@@ -24,11 +24,29 @@ import org.jmrtd.lds.icao.DG1File
 
 class NFCDocument {
 
+    /**
+     * Initiates the task to start reading the passport data using the provided IsoDep object and MRZ.
+     *
+     * @param isoDep The IsoDep object that represents the ISO-DEP (ISO 14443-4) communication interface.
+     * @param mrz The MRZ (Machine Readable Zone) data of the passport.
+     * @return A Result object that encapsulates the identity document data if successful, or an error if unsuccessful.
+     */
     fun startReadTask(isoDep: IsoDep, mrz: MRZ): Result<IdentityDocument, Error> {
         val bacKey = BACKey(mrz.documentNumber, mrz.dateOfBirth, mrz.dateOfExpiry)
         return readPassport(isoDep, bacKey)
     }
 
+    /**
+     * Reads the passport data using the provided IsoDep object and a BAC key.
+     * This function attempts to perform PACE (Password Authenticated Connection Establishment) using the generated PACE key,
+     * and falls back to BAC (Basic Access Control) if PACE fails.
+     *
+     * @param isoDep The IsoDep object that represents the ISO-DEP (ISO 14443-4) communication interface.
+     * @param bacKey The BAC key specification used for authentication if PACE fails.
+     *
+     * @return A Result object that encapsulates the identity document data if successful, or an error if unsuccessful.
+     * If successful, the Result contains an IdentityDocument object. If unsuccessful, the Result contains an Error object.
+     */
     private fun readPassport(isoDep: IsoDep, bacKey: BACKeySpec): Result<IdentityDocument, Error> {
         val paceKey = PACEKeySpec.createMRZKey(bacKey)
 
@@ -71,7 +89,13 @@ class NFCDocument {
         ).apply { open() }
     }
 
-    // Attempts to perform PACE and returns whether it succeeded
+        /**
+     * Performs Password Authenticated Connection Establishment (PACE) using the provided passport service and PACE key.
+     *
+     * @param passportService The PassportService object used for communication with the passport.
+     * @param paceKey The PACEKeySpec object containing the key for PACE authentication.
+     * @return True if PACE authentication is successful; false otherwise.
+     */
     private fun performPace(passportService: PassportService, paceKey: PACEKeySpec): Boolean {
         return runCatching {
             passportService.getInputStream(PassportService.EF_CARD_ACCESS).use { stream ->
@@ -97,7 +121,17 @@ class NFCDocument {
         }
     }
 
-    // Extracts the identity document information from the passport service
+    /**
+     * Extracts identity document data from the given passport service.
+     *
+     * This function reads the data from the passport service's specific files (DG1, DG11, and DG12),
+     * and extracts various pieces of information to create an IdentityDocument object. The extracted
+     * information includes the document type, document number, names, gender, issuing state, nationality,
+     * address, place of birth, birth date, expiration date, and delivery date.
+     *
+     * @param passportService The PassportService object that provides access to the passport data.
+     * @return An IdentityDocument object containing the extracted information.
+     */
     private fun extractIdentityDocument(passportService: PassportService): IdentityDocument {
         val dg1File = DG1File(passportService.getInputStream(PassportService.EF_DG1))
         val dg11File = DG11File(passportService.getInputStream(PassportService.EF_DG11))
@@ -128,6 +162,18 @@ class NFCDocument {
 }
 
 
+/**
+ * Extracts the last names from the provided input string.
+ *
+ * This function splits the input string by "<<" and takes the part after this delimiter.
+ * Then it further splits this part by "<" and filters out any empty parts. Finally, it joins
+ * the remaining parts with spaces to form the last names.
+ *
+ * @param input The input string from which the last names should be extracted. It is expected to
+ *              be in a specific format, containing "<<" and "<" as delimiters.
+ * @return The extracted last names as a string. If no last names are found in the input string,
+ *         an empty string is returned.
+ */
 fun extractLastNames(input: String): String {
     val parts = input.split("<<")
 
